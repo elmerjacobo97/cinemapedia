e6528ff0a5e51a2a7a5426ec1a8f5fa6d472245d
Elmer Jacobo <ejacobotiniano@gmail.com>
2023-07-30 22:39:38 -0500

    mostrar title movies

---
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -0,0 +1,30 @@
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
+import 'package:dio/dio.dart';
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+
+class MovieDbDataSource extends MoviesDataSource {
+  final dio = Dio(
+      BaseOptions(baseUrl: 'https://api.themoviedb.org/3', queryParameters: {
+    'api_key': Environment.theMovieDbKey,
+    'language': 'es-ES',
+  }));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get(
+      '/movie/now_playing',
+    );
+
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = movieDBResponse.results
+        .where((moviedb) =>
+            moviedb.posterPath != 'no-poster') // Filtrar películas sin póster
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
+
+    return movies;
+  }
+}
